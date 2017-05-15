02c518b9cdd99d8bb0fba60c390e8d0a6f0b7683
beebeean09 <beebeean09@gmail.com>
2017-05-14 22:17:10 -0700

    add new express postgres

---
+++ Project2/node-postgres-promises/queries.js
@@ -0,0 +1,17 @@
+const promise = require('bluebird');
+
+const options = {
+  promiseLab: promise
+};
+
+const pgp = require('pg-promise')(options);
+const connectionString = 'postgres://localhost:5432/puppies';
+const db = pgp(connectionString);
+
+// module.exports = {
+//   getAllPuppies: getAllPuppies,
+//   getSinglePuppy: getSinglePuppy,
+//   createPuppy: createPuppy,
+//   updatePuppy: updatePuppy,
+//   removePuppy: removePuppy
+// };
