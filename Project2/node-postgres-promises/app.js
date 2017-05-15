75d9025b4d8aff7a194011f6b3c602bad4dac9d5
beebeean09 <beebeean09@gmail.com>
2017-05-14 23:13:42 -0700

    complete queries for puppies and routes

--- Project2/node-postgres-promises/app.js
+++ Project2/node-postgres-promises/app.js
@@ -43,4 +43,26 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status( err.code || 500 )
+    .json({
+      status: 'error',
+      message: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500)
+  .json({
+    status: 'error',
+    message: err.message
+  });
+});
+
 module.exports = app;
